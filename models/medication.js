9fef6014ddaec05c790bb18f92299afda1c2f957
Ayush Jha <ayushjha@apples-MacBook-Pro.local>
2016-04-09 15:37:12 -0700

    removed extra schema name from /controller/medication.js

--- models/medication.js
+++ models/medication.js
@@ -1,10 +1,7 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+
 var MedicationReview = require('./medicationReview');
-
-var MedicationSchema = new Schema ({
-
-});
 
 var MedicationSchema = new Schema({
   drugName: String,
